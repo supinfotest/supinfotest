dfc82aa6d541898bd70685de90b9483276fd40d2
PAYOFFICE FR <dev2@novatecweb.com>
2013-05-28 18:21:31 -0600

    premier commit

---
+++ gen/com/supinfocaraibes/carwrecklocator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.supinfocaraibes.carwrecklocator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/supinfocaraibes/carwrecklocator/CarWreckDataForm.java
@@ -0,0 +1,15 @@
+package com.supinfocaraibes.carwrecklocator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarWreckDataForm extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.car_wreck_data_form_layout);
+	}
+
+}
